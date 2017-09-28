10845ae79ff4c8ac00e0f88770bc55ed4c71bde8
Anu Tomy <anuatomy@gmail.com>
2017-09-28 12:12:51 -0400

    Find documents based on geo-coordinates

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,13 +1,36 @@
-var dbconn = require('../data/dbconnection.js');
-var ObjectId = require('mongodb').ObjectID;
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
 
-var hotelData = require('../data/hotel-data.json');
+
+var runGeoQuery = function(req, res) {
+
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+
+  // A geoJSON point
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+
+  var geoOptions = {
+    spherical : true,
+    maxDistance : 2000,
+    num : 5
+  };
+
+  Hotel
+    .geoNear(point, geoOptions, function(err, results, stats) {
+      console.log('Geo Results', results);
+      console.log('Geo stats', stats);
+      res
+        .status(200)
+        .json(results);
+    });
+
+};
 
 module.exports.hotelsGetAll = function(req, res) {
-
-  var db = dbconn.get();
-
-  // console.log("db", db);
 
   console.log('GET the hotels');
   console.log(req.query);
@@ -15,7 +38,10 @@ module.exports.hotelsGetAll = function(req, res) {
   var offset = 0;
   var count = 5;
 
-  var collection = db.collection('hotels');
+  if (req.query && req.query.lat && req.query.lng) {
+    runGeoQuery(req, res);
+    return;
+  }
 
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
@@ -25,33 +51,29 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
-  collection
+  Hotel
     .find()
     .skip(offset)
     .limit(count)
-    .toArray(function(err, docs) {
-      console.log("Found hotels", docs.length);
+    .exec(function(err, hotels) {
+      console.log("Found hotels", hotels.length);
       res
-        .status(200)
-        .json(docs);
-  });
+        .json(hotels);
+    });
 
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-  var db = dbconn.get();
   var id = req.params.hotelId;
-  var collection = db.collection('hotels');
   console.log('GET hotelId', id);
 
-  collection
-    .findOne({
-      _id : ObjectId(id)
-    }, function(err, doc) {
+  Hotel
+    .findById(id)
+    .exec(function(err, doc) {
       res
         .status(200)
         .json(doc);
-  });
+    });
 
 };
 
@@ -79,7 +101,9 @@ module.exports.hotelsAddOne = function(req, res) {
     console.log("Data missing from body");
     res
       .status(400)
-      .json({ message : "Required data missing from body" });
+      .json({
+        message: "Required data missing from body"
+      });
   }
 
 };
