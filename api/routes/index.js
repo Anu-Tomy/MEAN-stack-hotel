6cb4f8148254449dc0da38425775922d8853e91e
Anu Tomy <anuatomy@gmail.com>
2017-09-28 17:51:06 -0400

    Tested update and delete methods using postman

--- api/routes/index.js
+++ api/routes/index.js
@@ -12,7 +12,9 @@ router
 
 router
   .route('/hotels/:hotelId')
-  .get(ctrlHotels.hotelsGetOne);
+  .get(ctrlHotels.hotelsGetOne)
+  .put(ctrlHotels.hotelsUpdateOne)
+  .delete(ctrlHotels.hotelsDeleteOne);
 
 
 // Review routes
@@ -23,6 +25,8 @@ router
 
 router
   .route('/hotels/:hotelId/reviews/:reviewId')
-  .get(ctrlReviews.reviewsGetOne);
+  .get(ctrlReviews.reviewsGetOne)
+  .put(ctrlReviews.reviewsUpdateOne)
+  .delete(ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;
